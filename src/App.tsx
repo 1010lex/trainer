d5bb43a7a1830a886aebd3c34b725e2fb8ca0f5b
Alexander Roland <aroland@gmx.de>
2022-02-28 22:45:49 +0100

    Refactor - component classes instead of functions - inject values via props instead of relying on global values

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Button from '@mui/material/Button'
 import ButtonGroup from '@mui/material/ButtonGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
-// import InputLabel from '@mui/material/InputLabel'
 import FormGroup from '@mui/material/FormGroup'
 import Stack from '@mui/material/Stack'
 import Switch from '@mui/material/Switch'
@@ -10,13 +9,73 @@ import Add from '@mui/icons-material/Add'
 import Remove from '@mui/icons-material/Remove'
 import './App.css'
 
-function App () {
-  type Exercise = {
-    id: number
-    name: string
-  }
+type ExerciseControlGroupProps = {
+  exercises: Exercise[]
+}
 
-  const exercises: Exercise[] = [
+class ExerciseControlGroup extends Component<ExerciseControlGroupProps> {
+  render () {
+    const { exercises } = this.props
+    return (
+      <FormGroup className='form-group-exercise'>
+        {
+          exercises.map((exercise) => {
+            return <FormControlLabel className='form-group-exercise--label' key={exercise.id} labelPlacement='start' control={<Switch className='form-group-exercise--switch' defaultChecked />} label={exercise.name} />
+          })
+        }
+      </FormGroup>
+    )
+  }
+}
+
+type IntervalButtonGroupProps = {
+  duration: number
+}
+
+class IntervalButtonGroup extends Component<IntervalButtonGroupProps> {
+  render () {
+    const { duration } = this.props
+    return (
+      <FormGroup className='form-group-interval'>
+        <FormControlLabel className='form-group-interval--label' labelPlacement='start' label={duration}
+          control={
+            <ButtonGroup className='form-group-interval--button-group' variant='contained'>
+              <Button><Remove /></Button>
+              <Button><Add /></Button>
+            </ButtonGroup>
+          }
+        />
+      </FormGroup>
+    )
+  }
+}
+
+type StartStopButtonGroupProps = {
+  started: boolean
+}
+
+class StartStopButtonGroup extends Component<StartStopButtonGroupProps> {
+  render () {
+    const { started } = this.props
+    return (
+      <ButtonGroup className='form-group-startstop--button-group' variant='contained'>
+        {
+          started
+            ? <Button color='error'>Stop</Button>
+            : <Button color='success'>Start</Button>
+        }
+      </ButtonGroup>
+    )
+  }
+}
+
+type Exercise = {
+  id: number
+  name: string
+}
+
+class App extends Component {
+  private exercises: Exercise[] = [
     { id: 0, name: '1 (Jab)' },
     { id: 1, name: '2 (Cross-Punch)' },
     { id: 2, name: '3 (Hook)' },
@@ -32,57 +91,21 @@ function App () {
     { id: 12, name: 'Pivot' },
   ]
 
-  const duration: number = 5000
+  private duration: number = 5000
 
-  const started: boolean = false
+  private started: boolean = false
 
-  function ExerciseControlGroup (props: { exercises: Exercise[] }) {
-    const { exercises } = props
+  render () {
     return (
-      <FormGroup className='form-group-exercise'>
-        {
-          exercises.map((exercise) => {
-            return <FormControlLabel className='form-group-exercise--label' key={exercise.id} labelPlacement='start' control={<Switch className='form-group-exercise--switch' defaultChecked />} label={exercise.name} />
-          })
-        }
-      </FormGroup>
-    )
-  }
-
-  function IntervalButtonGroup () {
-    return (
-      <FormGroup className='form-group-interval'>
-        <FormControlLabel className='form-group-interval--label' key={123} labelPlacement='start' label={duration} control={
-          <ButtonGroup className='form-group-interval--button-group' variant='contained'>
-            <Button><Remove /></Button>
-            <Button><Add /></Button>
-          </ButtonGroup>
-        } />
-      </FormGroup>
-    )
-  }
-
-  function StartStopButtonGroup () {
-    return (
-      <ButtonGroup className='form-group-startstop--button-group' variant='contained'>
-        {
-          started
-            ? <Button color='error'>Stop</Button>
-            : <Button color='success'>Start</Button>
-        }
-      </ButtonGroup>
-    )
-  }
-
-  return (
-    <Stack className="app-layout--wrapper" direction={'row'} alignItems='center' justifyContent={'center'}>
-      <Stack className="app-layout--main" direction={'column'} alignItems='center' justifyContent={'center'}>
-        <ExerciseControlGroup exercises={exercises} />
-        <IntervalButtonGroup />
-        <StartStopButtonGroup />
+      <Stack className="app-layout--wrapper" direction={'row'} alignItems='center' justifyContent={'center'}>
+        <Stack className="app-layout--main" direction={'column'} alignItems='center' justifyContent={'center'}>
+          <ExerciseControlGroup exercises={this.exercises} />
+          <IntervalButtonGroup duration={this.duration} />
+          <StartStopButtonGroup started={this.started} />
+        </Stack>
       </Stack>
-    </Stack>
-  )
+    )
+  }
 }
 
 export default App
